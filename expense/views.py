b8807931ba81a4bf218d11c482c2492c7a494f8e
1666sApple <ananno.034@gmail.com>
2024-08-20 03:24:23 +0600

    Update views.py

--- expense/views.py
+++ expense/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense
 from .forms import ExpenseForm
 
 def index(request):
+    expenses = Expense.objects.all()
     if request.method == "POST":
         expense_form = ExpenseForm(request.POST)
         if expense_form.is_valid():
             expense_form.save()
     else:
         expense_form = ExpenseForm()
+        
     
-    return render(request, 'expense/index.html', {'expense_form': expense_form})
+    return render(request, 'expense/index.html', 
+        {'expense_form': expense_form,
+            'expenses':expenses})
+
+def edit(request, id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method == "POST":
+        expense = Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        
+    return render(request, 
+        'expense/edit.html', 
+        {'expense_form':expense_form})
+    
+def delete(request, id):
+    if request.method == 'POST':
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
